3966b920e2cb25524869bdcb7bec0bbf109cb3cd
Juan Diego <39896844+juandiegovqdev@users.noreply.github.com>
2023-06-21 12:14:39 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def problema_del_agente_viajero():
+    print("Problema del Agente Viajero.")
+
+
+def problema_de_las_fases_semaforicas():
+    print("Problema de las Fases Semafóricas.")
+
+
+def problema_del_transporte():
+    print("Problema del Transporte.")
+
+
+def main():
+    print("¡Hola, Mundo!")
+
+
+if __name__ == "__main__":
+    main()
